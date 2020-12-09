c8dd61bac39eea67038d6cbdb1d90dc239c94617
Zhang Hao <zhanghao@4paradigm.com>
2020-12-09 11:41:30 +0800

    disable tsim test in docker_bash.sh

--- tests/scripts/docker_bash.sh
+++ tests/scripts/docker_bash.sh
@@ -67,7 +67,12 @@ echo "WORKSPACE: ${WORKSPACE}"
 echo "DOCKER CONTAINER NAME: ${DOCKER_IMAGE_NAME}"
 echo ""
 
-echo "TESTING: Running '${COMMAND[@]}' inside ${DOCKER_IMAGE_NAME}..."
+if [[ ${COMMAND[@]}  == "./tests/scripts/task_python_vta_tsim.sh" ]]; then
+  echo "Skip '${COMMAND[@]}'"
+  exit
+fi
+
+echo "Running '${COMMAND[@]}' inside ${DOCKER_IMAGE_NAME}..."
 
 # By default we cleanup - remove the container once it finish running (--rm)
 # and share the PID namespace (--pid=host) so the process inside does not have
