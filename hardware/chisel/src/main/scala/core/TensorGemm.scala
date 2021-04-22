ca013f448110b5e815c03ecefac076f746fa1a5d
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-04-22 15:53:22 -0700

    TensorGemm: explicitly define adder interface

--- hardware/chisel/src/main/scala/core/TensorGemm.scala
+++ hardware/chisel/src/main/scala/core/TensorGemm.scala
@@ -44,18 +44,22 @@ class MAC(aBits: Int = 8, bBits: Int = 8, cBits: Int = 16, flopIn: Boolean = fal
   io.y := addV
 }
 
+class AdderIO(val aBits: Int, val bBits: Int) extends Bundle {
+  val outBits = Math.max(aBits, bBits) + 1
+  val a = Input(SInt(aBits.W))
+  val b = Input(SInt(bBits.W))
+  val y = Output(SInt(outBits.W))
+}
+
+trait IsAdder { val io: AdderIO }
+
 /** PipeAdder
  *
  * This unit loads input bits into register and performs addition in the next cycle
  */
-class PipeAdder(aBits: Int = 8, bBits: Int = 8) extends Module {
-  val outBits = Math.max(aBits, bBits) + 1
-  val io = IO(new Bundle {
-    val a = Input(SInt(aBits.W))
-    val b = Input(SInt(bBits.W))
-    val y = Output(SInt(outBits.W))
-  })
-  val add = Wire(SInt(outBits.W))
+class PipeAdder(aBits: Int = 8, bBits: Int = 8) extends Module with IsAdder {
+  val io = IO(new AdderIO(aBits, bBits))
+  val add = Wire(chiselTypeOf(io.y))
   val rA = RegNext(io.a)
   val rB = RegNext(io.b)
   add := rA +& rB
@@ -67,14 +71,9 @@ class PipeAdder(aBits: Int = 8, bBits: Int = 8) extends Module {
  * This unit wires input bits to an adder directly.
  * The output comes out of combinational logic without waiting for another cycle.
  */
-class Adder(aBits: Int = 8, bBits: Int = 8) extends Module {
-  val outBits = Math.max(aBits, bBits) + 1
-  val io = IO(new Bundle {
-    val a = Input(SInt(aBits.W))
-    val b = Input(SInt(bBits.W))
-    val y = Output(SInt(outBits.W))
-  })
-  val add = Wire(SInt(outBits.W))
+class Adder(aBits: Int = 8, bBits: Int = 8) extends Module with IsAdder {
+  val io = IO(new AdderIO(aBits, bBits))
+  val add = Wire(chiselTypeOf(io.y))
   val rA = Wire(SInt(aBits.W))
   val rB = Wire(SInt(bBits.W))
   rA := io.a
